f831c04fffccbffb85f7f5af3a7e0de47b90d398
tests <mariobrusarosco@gmail.com>
2016-10-09 11:01:51 -0300

    PDO TEST

--- index.php
+++ index.php
@@ -1,29 +1,52 @@
 <?php
-//   HEADER   //
-require_once("includes/header.html");
+  // if(isset($_POST['submit'])) {
+  //   $username = $_POST['username'];
+  //   $password = $_POST['password'];
+  //
+  //   echo $username;
+  //   echo $password;
+  $connection = mysqli_connect();
+    // mysqli_connect("localhost","u845380189_eniac","eniac2016","u845380189_sint");
+ // }
+
+?>
 
 
- ?>
-<body>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <title>GUIA DE SINTAXES</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/guia-de-sintaxes.css">
+  </head>
+  <body>
 
-  <main>
+    <main class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12">
+          <h1>guia de sintaxes</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-md-6">
+          <form class="" id="" action="index.php" method="post">
+              <div class="form-group">
+                <input type="text" id="" class="form-control" name="username">
+              </div>
 
-  <?php
-    //    NAVIGATION   //
-    require_once('includes/navigation.html');
+              <div class="form-group">
+                <input type="password" id="" class="form-control" name="password">
+              </div>
 
-    //TABLE OF A SEARCH RESULTS//
-    require_once("includes/table-results.html");
-  ?>
-
-  </main>
-   <!-- //   FOOTER // -->
-
-   <!--  jQuery-->
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-   <!-- MY SCRIPT -->
-   <script src="js/syntaxes.js"></script>
-   <!-- <script src="js/elastic.js"></script> -->
+              <div class="form-group">
+                <input type="submit" id="" class="btn btn-block btn-primary" name="submit">
+              </div>
+          </form>
+        </div>
+      </div>
+    </main>
+    <!-- <script src="js/jquery..."></script> -->
   </body>
-
 </html>
